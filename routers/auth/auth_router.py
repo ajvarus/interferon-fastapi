d4d7dce356754f708485b9df0d75e6c92b35a363
Arjun Sen <arjunsen1997@gmail.com>
2024-06-02 20:10:46 +0100

    Changed cookie settings to work with third-party requests.

--- routers/auth/auth_router.py
+++ routers/auth/auth_router.py
@@ -15,18 +15,17 @@ router: APIRouter = APIRouter()
 
 @router.post("/")
 async def auth(
-    ar: AuthRequest, 
-    request: Request, 
-    response: Response, 
+    ar: AuthRequest,
+    request: Request,
+    response: Response,
     asi: AuthSessionInterface = Depends(get_auth_session_interface),
-    token = Cookie(None)
+    token=Cookie(None),
 ) -> UserSession:
     # Handle Signup
     try:
         if ar.auth_type == AuthType.SIGNUP:
             credentials: SignUpCredentials = SignUpCredentials(
-                email=ar.email,
-                password=ar.password
+                email=ar.email, password=ar.password
             )
             session: UserSession = await asi.signup_and_start_session(credentials)
             if not session.is_default() and session.is_active:
@@ -34,37 +33,38 @@ async def auth(
                     key="token",
                     value=session.token,
                     httponly=True,
+                    samesite="none",
                     secure=request.url.scheme == "https",
                     max_age=int(float(session.expiry) - datetime.now().timestamp()),
-                    path="/"
+                    path="/",
                 )
                 return session
             elif not session.is_default() and session.is_active == False:
                 return session
             return UserSession()
-        
+
         # Handle Signin
         if ar.auth_type == AuthType.SIGNIN:
             credentials: SignUpCredentials = SignUpCredentials(
-                email=ar.email,
-                password=ar.password
+                email=ar.email, password=ar.password
             )
             session: UserSession = await asi.login_and_start_session(credentials)
             if not session.is_default() and session.is_active:
-                    response.set_cookie(
-                        key="token",
-                        value=session.token,
-                        httponly=True,
-                        secure=request.url.scheme == "https",
-                        max_age=int(float(session.expiry) - datetime.now().timestamp()),
-                        path="/"
-                    )
-                    return session
+                response.set_cookie(
+                    key="token",
+                    value=session.token,
+                    httponly=True,
+                    samesite="none",
+                    secure=request.url.scheme == "https",
+                    max_age=int(float(session.expiry) - datetime.now().timestamp()),
+                    path="/",
+                )
+                return session
             elif not session.is_default() and session.is_active == False:
                 return session
             return UserSession()
-        
-        # Handle Signout 
+
+        # Handle Signout
         if ar.auth_type == AuthType.SIGNOUT:
             if token is not None:
                 session: UserSession = await asi.logout_and_terminate_session(token)
@@ -74,6 +74,5 @@ async def auth(
             return UserSession()
 
     except Exception as e:
-            print(str(e))
-            return UserSession()
-
+        print(str(e))
+        return UserSession()
